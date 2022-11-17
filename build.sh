0b1b40843c1fcbe68dc9884cbaa128e05f692a8b
wilmendoza <117107186+wilmendoza@users.noreply.github.com>
2022-11-17 14:55:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ PHALCON_VERSION=3.4.x
 PHP_VERSION=7.2
 
 
-docker build --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHALCON_VERSION=$PHALCON_VERSION -t phalcon_test .
+docker build --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHALCON_VERSION=$PHALCON_VERSION -t phalcon:3.4 .
