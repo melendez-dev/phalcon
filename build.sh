b6c17387da9e8c53c6c17825993b6dd133ae71d7
wilmendoza <wilson.mendoza@bodytechcorp.com>
2023-01-11 10:27:49 -0500

    adding xdebug

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PHALCON_VERSION=3.4.x
 PHP_VERSION=7.2
+IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
 
-
-docker build --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHALCON_VERSION=$PHALCON_VERSION -t phalcon:3.4 .
+docker build --build-arg IP=$IP --build-arg PHP_VERSION=$PHP_VERSION --build-arg PHALCON_VERSION=$PHALCON_VERSION -t phalcon:3.4-xdebug .
