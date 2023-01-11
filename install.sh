8aa62b1d3b4be05a70b660f832a1a164509bc7f4
wilmendoza <wilson.mendoza@bodytechcorp.com>
2023-01-11 09:35:39 -0500

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+PHALCON_PATH="$HOME/phalcon"
+PROJECTS_FOLDER_PATH="$HOME/Desktop/BODYTECH_PROJECTS"
+GITHUB_ORG="git@github.com:BodyTech-Dev"
+PROJECTS=(
+    "API-WEBPERU-v2"
+    "API-WEBATH-v2"
+)
+
+./build.sh
+
+if [[ ! -d $PROJECTS_FOLDER_PATH ]];
+then
+    mkdir $PROJECTS_FOLDER_PATH
+fi
+
+if [[ ! -d $PHALCON_PATH ]];
+then
+    mkdir $PHALCON_PATH
+fi
+
+cp ./docker-compose.yml $PHALCON_PATH/docker-compose.yml
+docker-compose -f $PHALCON_PATH/docker-compose.yml up -d
+
+if [[ ! -d $PHALCON_PATH/projects ]];
+then
+    ln -s $PROJECTS_FOLDER_PATH $PHALCON_PATH/projects
+fi
+
+for project in "${PROJECTS[@]}"
+do
+    DOMAIN=$(echo $project | tr '[:upper:]' '[:lower:]')
+    DOMAIN=$(echo $DOMAIN | sed -E -e 's/api\-|-v2//g')
+
+    echo $DOMAIN
+
+    if [[ -d $PROJECTS_FOLDER_PATH/$project ]];
+    then
+        cd $PROJECTS_FOLDER_PATH/$project
+        git pull
+    else
+        git clone $GITHUB_ORG/$project.git -b develop $PROJECTS_FOLDER_PATH/$project
+        if ! grep "/$DOMAIN.local/" /etc/hosts;
+        then
+            echo -e "127.0.0.1\t$DOMAIN.local" | sudo tee -a /etc/hosts
+        fi
+    fi
+
+    if [[ ! -f $PHALCON_PATH/httpd/vhosts/$DOMAIN.local.conf ]];
+    then
+        echo -e "<VirtualHost *:80>
+        ServerName $DOMAIN.local
+        DocumentRoot /var/www/html/$project/application
+
+        ErrorLog /var/log/apache2/$DOMAIN.local-error.log
+        CustomLog /var/log/apache2/$DOMAIN.local-access.log combined
+
+        <Directory /var/www/html/$project>
+            Options Indexes FollowSymLinks MultiViews
+            AllowOverride All
+            Require all granted
+        </Directory>
+        </VirtualHost>" | sudo tee $PHALCON_PATH/httpd/vhosts/$DOMAIN.local.conf
+    fi
+done
+
+docker restart phalcon3
